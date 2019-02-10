56714b70d7528d858cb1bbe2824047a97f9d02ae
Igor <igorpleskach@gmail.com>
2019-02-10 14:40:04 +0200

    Update Facebook.java

--- Facebook.java
+++ Facebook.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Facebook {
-public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\Selenium webdriver\\chrome\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver(); 
-	driver.get("https://uk-ua.facebook.com");
-}
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\Selenium webdriver\\chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver(); 
+		driver.get("https://uk-ua.facebook.com");
+	}
 }
